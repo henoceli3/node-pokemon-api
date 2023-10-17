fb13b2111983d4f25635938d1d3d9b935b72bd31
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2023-10-17 11:54:10 +0000

    Update dependencies, add bcrypt and token, modify database connection and models

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,51 +1,55 @@
-const { Sequelize, DataTypes } = require('sequelize')
-const PokemonModel = require('../models/pokemon')
-const UserModel = require('../models/user')
-const pokemons = require('./mock-pokemon')
-const bcrypt = require('bcrypt')
-  
+const { Sequelize, DataTypes } = require("sequelize");
+const PokemonModel = require("../models/pokemon");
+const UserModel = require("../models/user");
+const pokemons = require("./mock-pokemon");
+const bcrypt = require("bcrypt");
+
 /*connexion a la base donées */
-const sequelize = new Sequelize('pokedex', 'root', '', {
-  host: 'localhost',
-  dialect: 'mariadb',
+const sequelize = new Sequelize("pokedex", "root", "", {
+  host: "localhost",
+  dialect: "mariadb",
   dialectOptions: {
-    timezone: 'Etc/GMT-2',
+    timezone: "Etc/GMT-2",
   },
-  logging: false
-})
+  logging: false,
+});
 
 /*creation du modele de notre bd */
-const Pokemon = PokemonModel(sequelize, DataTypes)
-const User = UserModel(sequelize, DataTypes)
+const Pokemon = PokemonModel(sequelize, DataTypes);
+const User = UserModel(sequelize, DataTypes);
 
 /*synchronisation de notre bd*/
-const initDb = () => {
-  return sequelize.sync({force: true}).then(_ => {
-    pokemons.map(pokemon => {
-      Pokemon.create({
-        name: pokemon.name,
-        hp: pokemon.hp,
-        cp: pokemon.cp,
-        picture: pokemon.picture,
-        types: pokemon.types
-      }).then(pokemon => console.log(pokemon.toJSON()))
-    })
-
-    
-    bcrypt.hash('pikachu',10)
-    .then(hash => User.create(
+const initDb = async () => {
+  const _ = await sequelize.sync({ force: true });
+  pokemons.map((pokemon) => {
+    Pokemon.create({
+      name: pokemon.name,
+      hp: pokemon.hp,
+      cp: pokemon.cp,
+      picture: pokemon.picture,
+      types: pokemon.types,
+    }).then((pokemon_1) => console.log(pokemon_1.toJSON()));
+  });
+  bcrypt
+    .hash("pikachu", 10)
+    .then((hash) => User.create(
       {
-        username:'pikachu',
-        password:hash,
+        username: "pikachu",
+        password: hash,
+      },
+      {
+        username: "twitter",
+        password: hash,
       }
-    ))
-    .then(user => console.log(user.toJSON()))
-
-    console.log('La base de donnée a bien été initialisée !')
-  })
-}
+    )
+    )
+    .then((user) => console.log(user.toJSON()));
+  console.log("La base de donnée a bien été initialisée !");
+};
 
 /*on exporte la function initDB et le modele pokemon */
-module.exports = { 
-  initDb, Pokemon, User
-}
+module.exports = {
+  initDb,
+  Pokemon,
+  User,
+};
